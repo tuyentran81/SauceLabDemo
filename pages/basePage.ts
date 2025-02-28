09b1cd5a55d13f0a671b95a2d939f1985517bb90
Tuyen Tran <tuyenonline@gmail.com>
2025-02-28 16:44:49 +0700

    Add basePage file.

---
+++ pages/basePage.ts
@@ -0,0 +1,15 @@
+import { Page } from "@playwright/test";
+
+class BasePage {
+    protected readonly page: Page;
+
+    constructor(options: { page: Page }) {
+        this.page = options.page;
+    }
+
+    async goto(url: string) {
+        await this.page.goto(url);
+    }
+}
+
+export default BasePage;
